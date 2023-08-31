3f136c7dbe9e14c6093eb85c2e9b7b42699f2c5f
SATYAM <126665079+Raichuop07@users.noreply.github.com>
2023-08-31 09:50:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,10 +80,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/ba43f5de1ac7f8541c550.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/931635325e67b1b348ab2.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/ba43f5de1ac7f8541c550.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/931635325e67b1b348ab2.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
